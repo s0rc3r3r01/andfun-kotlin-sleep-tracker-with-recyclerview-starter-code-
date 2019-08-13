ed1bfa71b0b44ae1345487585441dac338054b9b
Federico Fregosi <federico@surfer-172-29-12-249-hotspot.internet-for-guests.com>
2019-08-13 21:46:57 +0100

    First commit

---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+//class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>()
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+
+    var data = listOf<SleepNight>()
+
+    set(value){
+        field = value
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        if (item.sleepQuality <= 1){
+            holder.textView.setTextColor(Color.RED)
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
+
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    val layoutInflater = LayoutInflater.from(parent.context)
+    val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+    return TextItemViewHolder(view)
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val sleepQuality: TextView = itemView.findViewById(R.id.sleep_length)
+    }
+}
